dec83e350ab3155ec7762ab5ed33eaeb48145141
hiba10 <hiba.chetali@esprit.tn>
2018-11-08 11:49:19 +0400

    hello2
    fixed

---
+++ Map-Thinkers/MapData/Migrations/201811080748216_db.cs
@@ -0,0 +1,19 @@
+namespace MapData.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class db : DbMigration
+    {
+        public override void Up()
+        {
+
+
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
